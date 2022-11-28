c5d076c54b328285742398e0ac9e99c572a09c4c
tomkovich <bayern_munchen22@mail.ru>
2022-11-28 23:47:04 +0100

    Finish the project

---
+++ js/single.js
@@ -0,0 +1,101 @@
+import { renderCurrencyItem } from "./markups.js";
+import state from "./state.js";
+import variables from "./variables.js";
+
+const { success, currentCurrency, currentCurrencyList } = variables;
+
+const insertCurrency = (data) => {
+  currentCurrencyList.insertAdjacentHTML(
+    "afterbegin",
+    renderCurrencyItem(data)
+  );
+};
+
+const insertCurrencies = () => {
+  const { currency, currencies } = state;
+  const { conversion_rates: rates, base_code: baseCode } = currency;
+
+  currentCurrency.innerHTML = renderCurrencyItem(currency);
+  currentCurrencyList.innerHTML = "";
+
+  Object.entries(rates).forEach(([code, rate]) => {
+    if (code === baseCode || !currencies.includes(code)) return;
+    insertCurrency({ ...currency, code, rate });
+  });
+};
+
+export const fetchLatest = async () => {
+  const {
+    url,
+    currency: { code },
+  } = state;
+
+  if (!code) return;
+
+  try {
+    const response = await fetch(`${url}/latest/${code}`);
+    const data = await response.json();
+
+    if (data.result === success) {
+      state.currency = { ...state.currency, ...data };
+      insertCurrencies();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const removeCurrency = (target) => {
+  const parent = target.parentElement.parentElement;
+  const { item } = parent.dataset;
+
+  if (!item) return;
+
+  const element = document.querySelector(`[data-item="${item}"]`);
+  element.remove();
+};
+
+const changeCurrency = () => {
+  currentCurrency.parentElement.classList.add("active");
+};
+
+export const handleActionClick = ({ target }) => {
+  const { action } = target.dataset;
+
+  if (!action) return;
+
+  const {
+    actions: { remove },
+  } = state;
+
+  action === remove ? removeCurrency(target) : changeCurrency();
+};
+
+export const handleSingleSelectChange = ({ target }) => {
+  target.parentElement.classList.remove("active");
+  state.currency = { ...state.currency, code: target.value };
+  fetchLatest();
+  target.value = "";
+};
+
+export const addCurrency = ({ currentTarget }) => {
+  currentTarget.parentElement.classList.add("active");
+};
+
+export const handleAddSelectChange = ({ target }) => {
+  const {
+    currency: { conversion_rates: rates, base_code: baseCode },
+  } = state;
+
+  const currency = Object.entries(rates).find(
+    ([key]) => key === target.value && key !== baseCode
+  );
+
+  if (currency) {
+    const [code, amount] = currency;
+    insertCurrency({ ...state.currency, code, rate: amount });
+  }
+
+  target.parentElement.classList.remove("active");
+  target.value = "";
+};
---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: "https://v6.exchangerate-api.com/v6/{API KEY}",
+  codes: [],
+  pair: {
+    from: "",
+    to: "",
+  },
+  amount: "",
+  loading: false,
+  currentTab: "convert",
+  currency: {
+    code: "USD",
+  },
+  currencies: ["USD", "EUR", "BYN"],
+  actions: {
+    remove: "remove",
+    change: "change",
+  },
+};
